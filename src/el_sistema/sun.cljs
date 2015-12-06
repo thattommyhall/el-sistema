f7ba0918fe33facc7bfef61fb34645d091ef9255
Jamie Brandon <jamie@scattered-thoughts.net>
2015-12-06 12:30:50 +0000

    Separate drawing from calculation

--- src/el_sistema/sun.cljs
+++ src/el_sistema/sun.cljs
@@ -4,24 +4,6 @@
             [goog.string.format])
   )
 
-(enable-console-print!)
-
-(def num_ids 10)
-
-(def lines
-  (vec
-   (for [i (range 10)]
-    [i (rand-int 500) (rand-int 500) (rand-int 500) (rand-int 500)])))
-
-(def edges
-  [[-1 0 0 0 500]
-   [-1 0 500 500 500]
-   [-1 500 500 500 0]
-   [-1 500 0 0 0]])
-
-(def all-lines
-  (vec (concat lines edges)))
-
 (defn line-intersection [[_ p0_x p0_y p1_x p1_y] [_ p2_x p2_y p3_x p3_y]]
   (let [s1_x (- p1_x p0_x)
         s1_y (- p1_y p0_y)
@@ -42,19 +24,10 @@
            d)]
     (let [ix (+ p0_x (* t s1_x))
           iy (+ p0_y (* t s1_y))]
-;;       (q/fill 0)
-;;       (q/text (goog.string/format "%.6f %.6f" s t) ix iy (+ ix 100) (+ iy 100))
       (when (and (> s 0) (< s 1) (> t 0) (< t 1))
         [s t ix iy]))))
 
-(def intersections
-  (vec
-   (filter identity
-    (for [line0 all-lines
-          line1 all-lines]
-      (line-intersection line0 line1)))))
-
-(defn ray-intersection [[p0_x p0_y p1_x p1_y] [_ p2_x p2_y p3_x p3_y]]
+(defn ray-intersection [[p0_x p0_y] [p1_x p1_y] [_ p2_x p2_y p3_x p3_y]]
   (let [s1_x (- p1_x p0_x)
         s1_y (- p1_y p0_y)
         s2_x (- p3_x p2_x)
@@ -74,23 +47,21 @@
            d)]
     (let [ix (+ p0_x (* t s1_x))
           iy (+ p0_y (* t s1_y))]
-;;       (q/fill 0)
-;;       (q/text (goog.string/format "%.6f %.6f" s t) ix iy (+ ix 100) (+ iy 100))
       (when (and (> s 0) (< s 1) (> t 0))
         [s t ix iy]))))
 
-(defn closest-intersection [ray lines]
+(defn closest-intersection [ray-from ray-to lines]
   (reduce
    (fn [[id-min s-min t-min ix-min iy-min] line]
-     (if-let [[s t ix iy] (ray-intersection ray line)]
+     (if-let [[s t ix iy] (ray-intersection ray-from ray-to line)]
        (if (< t t-min)
          [(line 0) s t ix iy]
          [id-min s-min t-min ix-min iy-min])
        [id-min s-min t-min ix-min iy-min]))
-   [-1 0 js/Number.POSITIVE_INFINITY (ray 2) (ray 3)]
+   [-1 0 js/Number.POSITIVE_INFINITY (ray-to 0) (ray-to 1)]
    lines))
 
-(defn rotate [angle [x y] [sx sy]]
+(defn rotate [angle [sx sy] [x y]]
   (let [a (q/cos angle)
         b (- (q/sin angle))
         c (- b)
@@ -100,37 +71,6 @@
         nx (+ (* a vx) (* b vy))
         ny (+ (* c vx) (* d vy))]
     [(+ sx nx) (+ sy ny)]))
-
-(defn impacts [[sx sy] lines intersections]
-  (vec
-   (concat
-    (for [[_ x0 y0 x1 y1] lines]
-      (let [[px py] (rotate 0.0001 [x0 y0] [sx sy])
-            [id _ _ ix iy] (closest-intersection [sx sy px py] all-lines)]
-        [id ix iy]))
-    (for [[_ x0 y0 x1 y1] lines]
-      (let [[px py] (rotate -0.0001 [x0 y0] [sx sy])
-            [id _ _ ix iy] (closest-intersection [sx sy px py] all-lines)]
-        [id ix iy]))
-        (for [[_ x0 y0 x1 y1] lines]
-      (let [[px py] (rotate 0.0001 [x1 y1] [sx sy])
-            [id _ _ ix iy] (closest-intersection [sx sy px py] all-lines)]
-        [id ix iy]))
-    (for [[_ x0 y0 x1 y1] lines]
-      (let [[px py] (rotate -0.0001 [x1 y1] [sx sy])
-            [id _ _ ix iy] (closest-intersection [sx sy px py] all-lines)]
-        [id ix iy]))
-    (for [[_ _ x0 y0] intersections]
-      (let [[px py] (rotate 0.0001 [x0 y0] [sx sy])
-            [id _ _ ix iy] (closest-intersection [sx sy px py] all-lines)]
-        [id ix iy]))
-    (for [[_ _ x0 y0] intersections]
-      (let [[px py] (rotate -0.0001 [x0 y0] [sx sy])
-            [id _ _ ix iy] (closest-intersection [sx sy px py] all-lines)]
-        [id ix iy]))
-    (for [[x y] [[0 0] [0 500] [500 0] [500 500]]]
-      (let [[id _ _ ix iy] (closest-intersection [sx sy x y] all-lines)]
-        [id ix iy])))))
 
 (defn compare-by-angle [[sx sy] [_ ax ay] [_ bx by]]
   (cond
@@ -163,46 +103,81 @@
         diff (js/Math.abs (- angle-a angle-b))]
   (min diff (- (* 2 js/Math.PI) diff))))
 
-(/ (angle-between [0 0] [nil 0 -0.1] [nil 0 0.1]) js/Math.PI)
-
-(defn draw []
-  (let [sun [(q/mouse-x) (q/mouse-y)]
-        impacts (impacts sun lines intersections)
+(defn calculate-sunlight [sun trees width height]
+  (let [[sx sy] sun
+        lines (vec (concat
+                    (for [[tree id] (map vector trees (range))
+                         [x0 y0 x1 y1] tree]
+                      [id x0 y0 x1 y1])
+                    [[-1 0 0 width 0]
+                     [-1 width 0 width height]
+                     [-1 width height 0 height]
+                     [-1 0 height 0 0]]))
+        intersections (vec
+                       (filter identity
+                               (for [line0 lines
+                                     line1 lines]
+                                 (line-intersection line0 line1))))
+        points (vec
+                (concat
+                 (for [[_ _ x y] intersections]
+                   [x y])
+                 (for [[_ x y _ _] lines]
+                   [x y])
+                 (for [[_ _ _ x y] lines]
+                   [x y])))
+        targets (vec
+                 (concat
+                  (for [point points]
+                    (rotate 0.0001 sun point))
+                  (for [point points]
+                    (rotate -0.0001 sun point))))
+        impacts (for [target targets]
+                  (let [[id _ _ x y] (closest-intersection sun target lines)]
+                    [id x y]))
         sorted-impacts (sort-by-angle sun impacts)
-        absorbs (make-array num-ids)]
-    (println "drawing at" (q/current-frame-rate))
-    (println sorted-impacts)
-    (q/background 255)
-    (q/fill 255 255 0)
-    (q/stroke 255 255 0)
-    (doseq [i (range (count sorted-impacts))]
-      (let [[_ x0 y0] (sorted-impacts i)
-            [_ x1 y1] (sorted-impacts (mod (+ i 1) (count sorted-impacts)))]
-        (q/triangle (sun 0) (sun 1) x0 y0 x1 y1)))
-    (q/stroke 0 0 0)
-;;     (doseq [[id ix iy] sorted-impacts]
-;;       (when (== id 0)
-;;         (q/line (sun 0) (sun 1) ix iy)))
-    (doseq [i (range num_ids)]
-      (aset absorbs i 0))
+        absorbs (make-array (count trees))]
+    (doseq [id (range (count trees))]
+      (aset absorbs id 0))
     (doseq [i (range (count sorted-impacts))]
       (let [[id0 x0 y0] (sorted-impacts i)
             [id1 x1 y1] (sorted-impacts (mod (+ i 1) (count sorted-impacts)))]
         (when (and (>= id0 0) (== id0 id1))
           (aset absorbs id0 (+ (aget absorbs id0) (angle-between sun [id0 x0 y0] [id1 x1 y1]))))))
-    (println (for [i (range (count sorted-impacts))]
-      (let [[id0 x0 y0] (sorted-impacts i)
-            [id1 x1 y1] (sorted-impacts (mod (+ i 1) (count sorted-impacts)))]
-        (when (and (>= id0 0) (== id0 id1))
-          (angle-between sun [id0 x0 y0] [id1 x1 y1])))))
-    (println absorbs)
-    (doseq [[id x0 y0 x1 y1] lines]
+    {:absorbs absorbs
+     :impacts sorted-impacts}))
+
+;; Rest of file is just for debugging
+
+(enable-console-print!)
+
+(def trees
+  (vec
+   (for [_ (range 10)]
+    [[(rand-int 500) (rand-int 500) (rand-int 500) (rand-int 500)]])))
+
+(defn draw []
+  (let [sun [(q/mouse-x) (q/mouse-y)]
+        {absorbs :absorbs impacts :impacts} (calculate-sunlight sun trees 500 500)]
+    (println "drawing at" (q/current-frame-rate))
+    (q/background 255)
+    (q/fill 255 255 0)
+    (q/stroke 255 255 0)
+    (doseq [i (range (count impacts))]
+      (let [[_ x0 y0] (impacts i)
+            [_ x1 y1] (impacts (mod (+ i 1) (count impacts)))]
+        (q/triangle (sun 0) (sun 1) x0 y0 x1 y1)))
+    (q/fill 0)
+    (q/stroke 0)
+    (q/ellipse (sun 0) (sun 1) 10 10)
+    (doseq [[tree id] (map vector trees (range))
+            [x0 y0 x1 y1] tree]
       (let [absorb (aget absorbs id)
             brightness (* absorb (/ 10 js/Math.PI))]
         (q/stroke 0 255 0)
         (q/line x0 y0 x1 y1)
-        (q/stroke 0)
-        (q/fill 0)
+        (q/stroke 255 0 0)
+        (q/fill 255 0 0)
         (q/ellipse x0 y0 brightness brightness)
         (q/ellipse x1 y1 brightness brightness)))
     (q/stroke 0 0 0)
